6cb1d86fa36da77c666a45c684c51866c72fc71f
Scott Johnston <scott@umachan.co.uk>
2021-03-12 20:28:28 +0000

    adding build folder

---
+++ tv-app/build/precache-manifest.aa0085eeb2348ce0aa3f1ff086039cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81299a7afbb4d02c803",
+    "url": "/static/css/main.3d929333.chunk.css"
+  },
+  {
+    "revision": "f81299a7afbb4d02c803",
+    "url": "/static/js/main.f81299a7.chunk.js"
+  },
+  {
+    "revision": "55362a39cf5c32639f19",
+    "url": "/static/js/1.55362a39.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e707c9aa29858992f7526849f5bd770",
+    "url": "/static/media/tv-shows-logo.3e707c9a.jpg"
+  },
+  {
+    "revision": "9a47d7bd41273e2ea70e42e10f56ec0c",
+    "url": "/index.html"
+  }
+];
