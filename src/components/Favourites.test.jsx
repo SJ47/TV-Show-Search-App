9b0d57a234db6232762cc03972a37f5ed379b478
Scott Johnston <scott@umachan.co.uk>
2023-03-07 21:01:34 +0000

    Fix test

--- src/components/Favourites.test.jsx
+++ src/components/Favourites.test.jsx
@@ -7,8 +7,7 @@ const favouriteShows = []
 describe('Favourites', () => {
   it('should render styles correctly', () => {
     const { container } = render(
-        // <Favourites favouriteShows={favouriteShows} onFavImageClicked={mockOnFavImageClicked}/>
-        <Favourites />
+        <Favourites favouriteShows={favouriteShows} onFavImageClicked={mockOnFavImageClicked}/>
     );
     expect(container).toMatchSnapshot();
   });
